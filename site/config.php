3a78334d86d2de7a98c912742badaafb0e105536
PatinhoDaShiro <81689830+PatinhoDaShiro@users.noreply.github.com>
2022-06-09 08:40:48 -0300

    Update config.php

--- site/config.php
+++ site/config.php
@@ -3,4 +3,4 @@ define("DIR_PATH", "C:\\xampp\htdocs\site");
 define("URL_BASE","http://localhost/site");
 define("TEMPLATE_BASE",DIR_PATH."/template");
 define("URL_TEMPLATE",URL_BASE."/template");
-define("GMAIL","patinhoprogramadoroficial@gmail.com");
+define("GMAIL","patinhoroficial@gmail.com");
